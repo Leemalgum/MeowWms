41b41a83c6ea6f751f40ae185b96d39470e7bc5e
hinata7010 <1234minseok@naver.com>
2024-03-20 18:50:50 +0900

    수정 필요 임시

--- src/main/java/com/ssg/MeowWms/controller/TestController.java
+++ src/main/java/com/ssg/MeowWms/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    @GetMapping("/common/fragments/header")
+    @GetMapping("/common/fragments/index")
     public void test(){
 
     }
