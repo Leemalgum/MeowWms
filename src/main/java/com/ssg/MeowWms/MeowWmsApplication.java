904673309f0997a5cf55934ca256d6d5f2853eb3
hinata7010 <1234minseok@naver.com>
2024-03-20 10:01:36 +0900

    First Commit
    First Commit

---
+++ src/main/java/com/ssg/MeowWms/MeowWmsApplication.java
@@ -0,0 +1,13 @@
+package com.ssg.MeowWms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeowWmsApplication.class, args);
+	}
+
+}
