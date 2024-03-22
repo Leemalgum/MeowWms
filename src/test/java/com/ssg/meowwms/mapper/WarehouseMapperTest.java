3e5b2eca6e576b788ba3ee50dab0bd000e377744
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-22 17:17:27 +0900

    test: WarehouseMapperTest 클래스 insert 테스트 구현

--- src/test/java/com/ssg/meowwms/mapper/WarehouseMapperTest.java
+++ src/test/java/com/ssg/meowwms/mapper/WarehouseMapperTest.java
@@ -1,5 +1,10 @@
 package com.ssg.meowwms.mapper;
 
+import com.ssg.meowwms.domain.WarehouseVO;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,5 +13,23 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
 class WarehouseMapperTest {
+
+    @Autowired
+    private WarehouseMapper warehouseMapper;
+
+    @Test
+    @DisplayName("창고 등록")
+    void insert() {
+        WarehouseVO warehouseVO = WarehouseVO.builder()
+                .name("야옹 창고")
+                .categoryId(1)
+                .managerId("WarehouseManager")
+                .volume(20L)
+                .latitude(37.517331925853)
+                .longitude(127.047377408384)
+                .build();
+
+        warehouseMapper.insert(warehouseVO);
+    }
 
 }
