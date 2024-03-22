bd63adfa67d1ba5741d87e0a59a79c17c5615114
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-22 16:03:37 +0900

    comment: WarehouseMapper 인터페이스 주석 추가

--- src/main/java/com/ssg/meowwms/mapper/WarehouseMapper.java
+++ src/main/java/com/ssg/meowwms/mapper/WarehouseMapper.java
@@ -2,6 +2,15 @@ package com.ssg.meowwms.mapper;
 
 import com.ssg.meowwms.domain.WarehouseVO;
 
+/**
+ * 창고 테이블과의 연결 작업을 수행합니다.
+ */
 public interface WarehouseMapper {
+
+    /**
+     * WarehouseVO에 클라이언트에서 입력 받은 정보를 담아 창고 테이블에 입력합니다.
+     *
+     * @param warehouseVO
+     */
     void insert(WarehouseVO warehouseVO);
 }
