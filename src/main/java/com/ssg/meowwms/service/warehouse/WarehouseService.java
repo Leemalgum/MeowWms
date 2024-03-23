91c10f1ff0bdc570f5fc8fe25e614dcf968f46bb
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-23 16:06:14 +0900

    feat: WarehouseService 인터페이스 register 메서드 구현

--- src/main/java/com/ssg/meowwms/service/warehouse/WarehouseService.java
+++ src/main/java/com/ssg/meowwms/service/warehouse/WarehouseService.java
@@ -1,4 +1,7 @@
 package com.ssg.meowwms.service.warehouse;
 
+import com.ssg.meowwms.domain.WarehouseVO;
+
 public interface WarehouseService {
+    public void register(WarehouseVO warehouseVO);
 }
