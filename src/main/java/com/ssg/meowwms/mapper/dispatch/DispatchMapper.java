9a8f8188a20e0fae02ec097235ab56cc65ac7e49
JiHyun Lee <supernova63@naver.com>
2024-03-23 15:51:30 +0900

    Fix : 수정 Mapper.java에 검색 기능 추가

--- src/main/java/com/ssg/meowwms/mapper/dispatch/DispatchMapper.java
+++ src/main/java/com/ssg/meowwms/mapper/dispatch/DispatchMapper.java
@@ -2,13 +2,17 @@ package com.ssg.meowwms.mapper.dispatch;
 
 import java.util.List;
 import com.ssg.meowwms.domain.dispatch.DispatchVO;
+import com.ssg.meowwms.domain.retrieval.ShippingOrdersVO;
+import com.ssg.meowwms.dto.OptionDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface DispatchMapper {
     void insertDispatch(DispatchVO dispatchVO);
     DispatchVO selectOneById(int id);
-    List<DispatchVO> selectAll();
+    List<DispatchVO> selectAll(@Param("options") List<OptionDTO> options);
     void updateDispatch(DispatchVO dispatchVO);
     void deleteDispatch(int id);
+    List<ShippingOrdersVO> search(@Param("options") List<OptionDTO> options);
 }
--- src/main/java/com/ssg/meowwms/mapper/retrieval/WaybillMapper.java
+++ src/main/java/com/ssg/meowwms/mapper/retrieval/WaybillMapper.java
@@ -1,7 +1,10 @@
 package com.ssg.meowwms.mapper.retrieval;
 
+import com.ssg.meowwms.domain.retrieval.ShippingOrdersVO;
 import com.ssg.meowwms.domain.retrieval.WaybillVO;
+import com.ssg.meowwms.dto.OptionDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -9,7 +12,8 @@ import java.util.List;
 public interface WaybillMapper {
     void insertWaybill(WaybillVO waybillVO);
     WaybillVO selectOneById(int id);
-    List<WaybillVO> selectAll();
+    List<WaybillVO> selectAll(@Param("options") List<OptionDTO> options);
     void updateWaybill(WaybillVO waybillVO);
     void deleteWaybill(int id);
+    List<ShippingOrdersVO> search(@Param("options") List<OptionDTO> options);
 }
