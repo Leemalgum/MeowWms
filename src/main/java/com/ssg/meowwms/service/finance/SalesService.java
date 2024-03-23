9673236c9654592de88300c1b4f79b52117ebb2c
hinata7010 <1234minseok@naver.com>
2024-03-23 15:53:55 +0900

    Add: SalesService 추가

---
+++ src/main/java/com/ssg/meowwms/service/finance/SalesService.java
@@ -0,0 +1,24 @@
+package com.ssg.meowwms.service.finance;
+
+import com.ssg.meowwms.dto.OptionDTO;
+import com.ssg.meowwms.dto.finance.SalesDTO;
+import com.ssg.meowwms.dto.finance.SalesMonthDTO;
+
+import java.util.List;
+
+public interface SalesService {
+    void insertSales(SalesDTO salesDTO);
+
+    SalesDTO selectSalesById(int id);
+
+    List<SalesDTO> selectAll(List<OptionDTO> optionList);
+
+    void deleteSales(int id);
+
+    void updateSales(SalesDTO salesDTO);
+
+    int sumSales(List<OptionDTO> optionList);
+
+    List<SalesMonthDTO> sumSalesByYear(int warehouseId, String year);
+
+}
