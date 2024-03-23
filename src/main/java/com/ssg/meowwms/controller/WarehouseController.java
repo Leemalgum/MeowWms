b2c03be54e2093eeea5681ec087dbfe8b080a43f
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-23 15:57:19 +0900

    feat: WarehouseController 구현

---
+++ src/main/java/com/ssg/meowwms/controller/WarehouseController.java
@@ -0,0 +1,12 @@
+package com.ssg.meowwms.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/warehouse")
+@RequiredArgsConstructor
+public class WarehouseController {
+
+}
