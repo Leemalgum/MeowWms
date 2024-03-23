d0810b2fbccf4b7f68395785aa194d04efbdf6a4
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-23 20:04:51 +0900

    feat: WarehouseMapper 인터페이스 selectAll 메서드 구현

--- src/main/java/com/ssg/meowwms/mapper/WarehouseMapper.java
+++ src/main/java/com/ssg/meowwms/mapper/WarehouseMapper.java
@@ -1,7 +1,11 @@
 package com.ssg.meowwms.mapper;
 
 import com.ssg.meowwms.domain.warehouse.WarehouseVO;
+import com.ssg.meowwms.dto.OptionDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 창고 테이블과의 연결 작업을 수행합니다.
@@ -23,4 +27,13 @@ public interface WarehouseMapper {
      * @return
      */
     WarehouseVO selectOne(int warehouseId);
+
+    /**
+     * 창고 테이블에서 전체 창고에 대한 정보를 가져와서 반환합니다.
+     * 검색 조건이 있을 경우 해당 검색 조건에 해당하는 창고들만 가져와서 반환합니다.
+     *
+     * @param optionList
+     * @return
+     */
+    List<WarehouseVO> selectAll(@Param("optionList")List<OptionDTO> optionList);
 }
