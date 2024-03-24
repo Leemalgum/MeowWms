f7e692ccc73b4f43c766c9cb96d5a73b9d6a0960
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-25 00:33:33 +0900

    comment: StorageController 주석 추가

--- src/main/java/com/ssg/meowwms/controller/storage/StorageController.java
+++ src/main/java/com/ssg/meowwms/controller/storage/StorageController.java
@@ -5,11 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 입고 관리
+ */
 @Controller
 @RequestMapping("/storage")
 @RequiredArgsConstructor
 public class StorageController {
 
+    /**
+     * 입고 요청 페이지를 불러옵니다.
+     *
+     * @return
+     */
     @GetMapping("/request")
     public String getRequest() {
         return "views/storage/request";
