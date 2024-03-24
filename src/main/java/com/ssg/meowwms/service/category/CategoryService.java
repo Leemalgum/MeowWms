40661400bd5230e389310d636b234cf5cf8cbda7
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-24 21:43:59 +0900

    feat: CategoryService 인터페이스 getAllCategories 메서드 구현

--- src/main/java/com/ssg/meowwms/service/category/CategoryService.java
+++ src/main/java/com/ssg/meowwms/service/category/CategoryService.java
@@ -1,4 +1,9 @@
 package com.ssg.meowwms.service.category;
 
+import com.ssg.meowwms.dto.category.CategoryDTO;
+
+import java.util.List;
+
 public interface CategoryService {
+    List<CategoryDTO> getAllCategories();
 }
