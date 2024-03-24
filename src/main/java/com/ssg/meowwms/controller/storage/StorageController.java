28e3e2401f3ab8a20885f0beb52e464d618ebac7
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-25 03:03:55 +0900

    feat: StorageController 클래스 ãgetMiddleCategories 메서드 구현

--- src/main/java/com/ssg/meowwms/controller/storage/StorageController.java
+++ src/main/java/com/ssg/meowwms/controller/storage/StorageController.java
@@ -1,12 +1,15 @@
 package com.ssg.meowwms.controller.storage;
 
 import com.ssg.meowwms.dto.category.MainCategoryDTO;
+import com.ssg.meowwms.dto.category.MiddleCategoryDTO;
 import com.ssg.meowwms.service.category.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,5 +38,21 @@ public class StorageController {
         model.addAttribute("mainCategories", mainCategories);
 
         return "views/storage/request";
+    }
+
+    /**
+     * 대분류에 해당하는 중분류 데이터를 가져옵니다.
+     *
+     * @param mainCategory 대분류
+     * @return 중분류 리스트
+     */
+    @GetMapping("/getMiddleCategories")
+    public @ResponseBody List<String> getMiddleCategories(@RequestParam("mainCategory") String mainCategory) {
+        List<String> middleCategories = new ArrayList<>();
+
+        List<MiddleCategoryDTO> middleCategoryDTOList = categoryService.getMiddleCategories(mainCategory);
+        middleCategoryDTOList.forEach(middleCategoryDTO -> middleCategories.add(middleCategoryDTO.getMiddleCategory()));
+
+        return middleCategories;
     }
 }
