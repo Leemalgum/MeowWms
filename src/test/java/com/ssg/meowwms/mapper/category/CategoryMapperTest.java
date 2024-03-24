414e386314c0f624c5fb4ce62ae65386f070d373
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-24 21:37:55 +0900

    test: CategoryMapperTest 클래스 selectAll 테스트 구현

--- src/test/java/com/ssg/meowwms/mapper/category/CategoryMapperTest.java
+++ src/test/java/com/ssg/meowwms/mapper/category/CategoryMapperTest.java
@@ -1,9 +1,14 @@
 package com.ssg.meowwms.mapper.category;
 
+import com.ssg.meowwms.domain.category.CategoryVO;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -12,4 +17,12 @@ public class CategoryMapperTest {
 
     @Autowired
     private CategoryMapper categoryMapper;
+
+    @Test
+    @DisplayName("카테고리 전체 조회")
+    void selectAll() {
+        List<CategoryVO> categories = categoryMapper.selectAll();
+
+        log.info(categories);
+    }
 }
