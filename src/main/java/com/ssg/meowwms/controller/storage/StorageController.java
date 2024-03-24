2a0a3c0f52664bf7a1abb1a952ff33f13aa7f37b
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-24 19:51:11 +0900

    feat: StroageController 구현

---
+++ src/main/java/com/ssg/meowwms/controller/storage/StorageController.java
@@ -0,0 +1,12 @@
+package com.ssg.meowwms.controller.storage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/storage")
+@RequiredArgsConstructor
+public class StorageController {
+
+}
