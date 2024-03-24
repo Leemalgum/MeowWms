ec935365ffe49517324bcfab276aa93851c33a5a
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-25 01:30:10 +0900

    refactor: StorageController 클래스 getRequest 메서드 에서 대분류 값을 입고 요청 페이지로 보내도록 로직 수정

--- src/main/java/com/ssg/meowwms/controller/storage/StorageController.java
+++ src/main/java/com/ssg/meowwms/controller/storage/StorageController.java
@@ -1,9 +1,15 @@
 package com.ssg.meowwms.controller.storage;
 
+import com.ssg.meowwms.dto.category.MainCategoryDTO;
+import com.ssg.meowwms.service.category.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 입고 관리
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/storage")
 @RequiredArgsConstructor
 public class StorageController {
+    private final CategoryService categoryService;
 
     /**
      * 입고 요청 페이지를 불러옵니다.
@@ -19,7 +26,14 @@ public class StorageController {
      * @return
      */
     @GetMapping("/request")
-    public String getRequest() {
+    public String getRequest(Model model) {
+        List<String> mainCategories = new ArrayList<>();
+
+        List<MainCategoryDTO> mainCategoryDTOList = categoryService.getMainCategories();
+        mainCategoryDTOList.forEach(mainCategoryDTO -> mainCategories.add(mainCategoryDTO.getMainCategory()));
+
+        model.addAttribute("mainCategories", mainCategories);
+
         return "views/storage/request";
     }
 }
