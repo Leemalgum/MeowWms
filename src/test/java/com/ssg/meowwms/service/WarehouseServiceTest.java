34b4add2d08babe4b40a0455f3d903b7b57f2047
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-24 16:32:45 +0900

    test: WarehouseServiceTest 클래스 getWarehouseDetail 테스트 구현

--- src/test/java/com/ssg/meowwms/service/WarehouseServiceTest.java
+++ src/test/java/com/ssg/meowwms/service/WarehouseServiceTest.java
@@ -2,6 +2,7 @@ package com.ssg.meowwms.service;
 
 import com.ssg.meowwms.dto.search.OptionDTO;
 import com.ssg.meowwms.dto.warehouse.WarehouseDTO;
+import com.ssg.meowwms.dto.warehouse.WarehouseDetailDTO;
 import com.ssg.meowwms.service.warehouse.WarehouseService;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.DisplayName;
@@ -50,5 +51,15 @@ public class WarehouseServiceTest {
         List<WarehouseDTO> warehouseDTOList = warehouseService.selectAll(optionList);
 
         log.info(warehouseDTOList);
+    }
+
+    @Test
+    @DisplayName("창고 상세 조회 테스트")
+    void getWarehouseDetail() {
+        int warehouseId = 1;
+
+        List<WarehouseDetailDTO> warehouseDetailList = warehouseService.getWarehouseDetail(warehouseId);
+
+        log.info(warehouseDetailList);
     }
 }
