bd100307003d7e9c34adb07903ae4ce01df9d05a
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-24 23:38:19 +0900

    test: CategoryServiceTest 클래스 getAllCategories 테스트 구현

--- src/test/java/com/ssg/meowwms/service/category/CategoryServiceTest.java
+++ src/test/java/com/ssg/meowwms/service/category/CategoryServiceTest.java
@@ -1,11 +1,28 @@
 package com.ssg.meowwms.service.category;
 
+import com.ssg.meowwms.dto.category.CategoryDTO;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootTest
 @Transactional
 @Log4j2
 public class CategoryServiceTest {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Test
+    @DisplayName("카테고리 전체 조회 서비스 테스트")
+    void getAllCategories() {
+        List<CategoryDTO> categories = categoryService.getAllCategories();
+
+        log.info(categories);
+    }
 }
