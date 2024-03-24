bcf7db7ad711f56a030b485c9acae9ceb030576d
JiHyun Lee <supernova63@naver.com>
2024-03-24 20:17:42 +0900

    Feat : 추가) RetrievalTimelineMapper 관련 java, xml, Tests

---
+++ src/test/java/com/ssg/meowwms/mapper/retrieval/RetrievalTimelineMapperTests.java
@@ -0,0 +1,56 @@
+package com.ssg.meowwms.mapper.retrieval;
+
+import com.ssg.meowwms.MeowWmsApplication;
+import com.ssg.meowwms.domain.retrieval.RetrievalTimelineVO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+//@Transactional
+public class RetrievalTimelineMapperTests {
+    @Autowired
+    private RetrievalTimelineMapper retrievalTimelineMapper;
+
+    @Test
+    void insertRetrievalTimelineTest() {
+        RetrievalTimelineVO retrievalTimelineVO = RetrievalTimelineVO.builder()
+                .id(2)
+                .requestTime(LocalDateTime.of(2024, 3, 25, 15, 26, 38))
+                .build();
+        retrievalTimelineMapper.insertRequestTime(retrievalTimelineVO);
+    }
+
+    @Test
+    void updateOrderTimeTest() {
+        RetrievalTimelineVO retrievalTimelineVO = RetrievalTimelineVO.builder()
+                .id(2)
+                .orderTime(LocalDateTime.of(2024, 3, 25, 16, 15, 26))
+                .build();
+        retrievalTimelineMapper.updateOrderTime(retrievalTimelineVO);
+    }
+
+    @Test
+    void updateWorkingTimeTest() {
+        RetrievalTimelineVO retrievalTimelineVO = RetrievalTimelineVO.builder()
+                .id(2)
+                .workingTime(LocalDateTime.of(2024, 3, 25, 16, 40, 36))
+                .build();
+        retrievalTimelineMapper.updateWorkingTime(retrievalTimelineVO);
+    }
+
+    @Test
+    void updateFinishedTimeTest() {
+        RetrievalTimelineVO retrievalTimelineVO = RetrievalTimelineVO.builder()
+                .id(2)
+                .finishedTime(LocalDateTime.of(2024, 3, 25, 17, 05, 18))
+                .build();
+        retrievalTimelineMapper.updateFinishedTime(retrievalTimelineVO);
+    }
+
+
+}
