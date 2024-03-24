4a38455489c6e219f70443936cb453b869d49248
JiHyun Lee <supernova63@naver.com>
2024-03-24 16:52:24 +0900

    Fix : 수정 MemberVO, MemberDTO co_reg_num 타입을 String으로 변경

--- src/main/java/com/ssg/meowwms/dto/user/MemberDTO.java
+++ src/main/java/com/ssg/meowwms/dto/user/MemberDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class MemberDTO {
-    private String uid;
+    private String uid; 
     private String shopName;
     private String coRegNum;
     private String shopAddress;
