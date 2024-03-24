d517e311e70a80681f615ee7ec5a414016afba20
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-24 23:34:57 +0900

    test: CategoryServiceTest 구현

---
+++ src/test/java/com/ssg/meowwms/service/category/CategoryServiceTest.java
@@ -0,0 +1,11 @@
+package com.ssg.meowwms.service.category;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@Log4j2
+public class CategoryServiceTest {
+}
