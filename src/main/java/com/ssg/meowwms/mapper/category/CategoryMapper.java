e9f40287aba14655b8bfc8c7f4070e93fa0b420c
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-25 00:36:14 +0900

    feat: CategoryMapper 인터페이스 selectMainCategories 메서드 구현

--- src/main/java/com/ssg/meowwms/mapper/category/CategoryMapper.java
+++ src/main/java/com/ssg/meowwms/mapper/category/CategoryMapper.java
@@ -1,6 +1,7 @@
 package com.ssg.meowwms.mapper.category;
 
 import com.ssg.meowwms.domain.category.CategoryVO;
+import com.ssg.meowwms.dto.category.MainCategoryDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -17,4 +18,11 @@ public interface CategoryMapper {
      * @return
      */
     List<CategoryVO> selectAll();
+
+    /**
+     * 카테고리 테이블에서 main_category를 중복 없이 가져옵니다.
+     *
+     * @return
+     */
+    List<MainCategoryDTO> selectMainCategories();
 }
