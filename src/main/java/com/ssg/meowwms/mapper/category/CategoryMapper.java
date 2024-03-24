d5c88cccf59ab0855e6a758cbf3a78dc226dbc8a
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-25 03:17:56 +0900

    feat: CategoryMapper 인터페이스 selectSubCategories 메서드 구현

--- src/main/java/com/ssg/meowwms/mapper/category/CategoryMapper.java
+++ src/main/java/com/ssg/meowwms/mapper/category/CategoryMapper.java
@@ -3,6 +3,7 @@ package com.ssg.meowwms.mapper.category;
 import com.ssg.meowwms.domain.category.CategoryVO;
 import com.ssg.meowwms.dto.category.MainCategoryDTO;
 import com.ssg.meowwms.dto.category.MiddleCategoryDTO;
+import com.ssg.meowwms.dto.category.SubCategoryDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -30,7 +31,17 @@ public interface CategoryMapper {
     /**
      * 카테고리 테이블에서 middle_category를 중복 없이 가져옵니다.
      *
+     * @param mainCategory
      * @return
      */
     List<MiddleCategoryDTO> selectMiddleCategories(String mainCategory);
+
+    /**
+     * 카테고리 테이블에서 sub_category를 중복 없이 가져옵니다.
+     *
+     * @param mainCategory
+     * @param middleCategory
+     * @return
+     */
+    List<SubCategoryDTO> selectSubCategories(String mainCategory, String middleCategory);
 }
