942c3d0397e5c8aabe33c430ce734603e1a523bf
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-25 03:28:29 +0900

    feat: CategoryService 인터페이스 getSubCategories 메서드 구현

--- src/main/java/com/ssg/meowwms/service/category/CategoryService.java
+++ src/main/java/com/ssg/meowwms/service/category/CategoryService.java
@@ -3,6 +3,7 @@ package com.ssg.meowwms.service.category;
 import com.ssg.meowwms.dto.category.CategoryDTO;
 import com.ssg.meowwms.dto.category.MainCategoryDTO;
 import com.ssg.meowwms.dto.category.MiddleCategoryDTO;
+import com.ssg.meowwms.dto.category.SubCategoryDTO;
 
 import java.util.List;
 
@@ -10,4 +11,5 @@ public interface CategoryService {
     List<CategoryDTO> getAllCategories();
     List<MainCategoryDTO> getMainCategories();
     List<MiddleCategoryDTO> getMiddleCategories(String mainCategory);
+    List<SubCategoryDTO> getSubCategories(String mainCategory, String middleCategory);
 }
