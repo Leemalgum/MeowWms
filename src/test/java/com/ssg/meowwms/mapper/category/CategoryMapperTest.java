7c4df208871591abfe57c4775933ffb14659148a
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-25 00:37:45 +0900

    test: CategoryMapperTest 클래스 selectMainCategories 테스트 구현

--- src/test/java/com/ssg/meowwms/mapper/category/CategoryMapperTest.java
+++ src/test/java/com/ssg/meowwms/mapper/category/CategoryMapperTest.java
@@ -1,6 +1,7 @@
 package com.ssg.meowwms.mapper.category;
 
 import com.ssg.meowwms.domain.category.CategoryVO;
+import com.ssg.meowwms.dto.category.MainCategoryDTO;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,5 +25,13 @@ public class CategoryMapperTest {
         List<CategoryVO> categories = categoryMapper.selectAll();
 
         log.info(categories);
+    }
+
+    @Test
+    @DisplayName("대분류를 중복 없이 조회")
+    void selectMainCategories() {
+        List<MainCategoryDTO> mainCategories = categoryMapper.selectMainCategories();
+
+        log.info(mainCategories);
     }
 }
