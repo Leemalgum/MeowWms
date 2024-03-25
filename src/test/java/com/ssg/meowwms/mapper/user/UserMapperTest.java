9b3eabdd71df7102c3f9f29e8023a4df3303de13
이맑음 <withxia1215@naver.com>
2024-03-25 17:26:35 +0900

    Fix: 임포트문 추가

--- src/test/java/com/ssg/meowwms/mapper/user/UserMapperTest.java
+++ src/test/java/com/ssg/meowwms/mapper/user/UserMapperTest.java
@@ -1,6 +1,7 @@
 package com.ssg.meowwms.mapper.user;
 
 import com.ssg.meowwms.domain.user.UserVO;
+import com.ssg.meowwms.mapper.user.UserMapper;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
