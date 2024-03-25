ad0c5fd003025e3df3dd19e3403f5a0b38753b0d
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-25 18:29:18 +0900

    feat: StorageController 클래스 requestStorage 메서드 구현

--- src/main/java/com/ssg/meowwms/controller/storage/StorageController.java
+++ src/main/java/com/ssg/meowwms/controller/storage/StorageController.java
@@ -4,17 +4,20 @@ import com.ssg.meowwms.dto.category.CategoryDTO;
 import com.ssg.meowwms.dto.category.MainCategoryDTO;
 import com.ssg.meowwms.dto.category.MiddleCategoryDTO;
 import com.ssg.meowwms.dto.category.SubCategoryDTO;
+import com.ssg.meowwms.dto.storage.ProductDTO;
+import com.ssg.meowwms.dto.storage.StockMovementDTO;
 import com.ssg.meowwms.dto.warehouse.WarehouseDTO;
 import com.ssg.meowwms.service.category.CategoryService;
+import com.ssg.meowwms.service.storage.StorageService;
 import com.ssg.meowwms.service.warehouse.WarehouseService;
+import com.ssg.meowwms.util.StockMovementStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +30,7 @@ import java.util.List;
 public class StorageController {
     private final CategoryService categoryService;
     private final WarehouseService warehouseService;
+    private final StorageService storageService;
 
     /**
      * 입고 요청 페이지를 불러옵니다.
@@ -80,6 +84,14 @@ public class StorageController {
         return subCategories;
     }
 
+    /**
+     * 대분류, 중뷴류, 소분류 값을 받아 일치하는 창고 목록을 반환합니다.
+     *
+     * @param mainCategory
+     * @param middleCategory
+     * @param subCategory
+     * @return 창고 목록
+     */
     @GetMapping("/getWarehouseWithCategory")
     public @ResponseBody List<WarehouseDTO> getWarehouseWithCategory(
             @RequestParam("mainCategory") String mainCategory,
@@ -90,5 +102,67 @@ public class StorageController {
         int categoryId = categoryDTO.getId();
 
         return warehouseService.getWarehouseWithCategory(categoryId);
+    }
+
+    /**
+     * 입고 요청을 처리합니다.
+     *
+     * @param productName
+     * @param mainCategory
+     * @param middleCategory
+     * @param subCategory
+     * @param warehouseId
+     * @param productBrand
+     * @param productPrice
+     * @param productSalePrice
+     * @param productQuantity
+     * @param productVolume
+     * @return
+     */
+    @PostMapping("request")
+    @Transactional(rollbackFor = Exception.class)
+    public String requestStorage(
+            @RequestParam("productName") String productName,
+            @RequestParam("mainType") String mainCategory,
+            @RequestParam("middleType") String middleCategory,
+            @RequestParam("subType") String subCategory,
+            @RequestParam("warehouseId") int warehouseId,
+            @RequestParam("productBrand") String productBrand,
+            @RequestParam("productPrice") int productPrice,
+            @RequestParam("productSalePrice") int productSalePrice,
+            @RequestParam("productQuantity") int productQuantity,
+            @RequestParam("productVolume") int productVolume
+    ) {
+
+        try {
+            int categoryId = categoryService.getWithCategories(mainCategory, middleCategory, subCategory).getId();
+
+            ProductDTO productDTO = ProductDTO.builder()
+                    .categoryId(categoryId)
+                    .name(productName)
+                    .brand(productBrand)
+                    .price(productPrice)
+                    .salePrice(productSalePrice)
+                    .quantity(productQuantity)
+                    .volume(productVolume)
+                    .userId("jongwoo")
+                    .build();
+
+            int productId = storageService.registerProduct(productDTO);
+
+            StockMovementDTO stockMovementDTO = StockMovementDTO.builder()
+                    .productId(productId)
+                    .userId("jongwoo")
+                    .statusCode(StockMovementStatus.REQUESTED.getCode())
+                    .requestDatetime(LocalDate.now())
+                    .warehouseId(warehouseId)
+                    .build();
+
+            storageService.registerStorage(stockMovementDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/";
     }
 }
