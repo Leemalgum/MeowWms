8cd40331c7f1101403262ef95619363995508f4c
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-25 14:25:32 +0900

    feat: StroageController 클래스 getWarehouseWithCategory 메서드 구현

--- src/main/java/com/ssg/meowwms/controller/storage/StorageController.java
+++ src/main/java/com/ssg/meowwms/controller/storage/StorageController.java
@@ -1,9 +1,12 @@
 package com.ssg.meowwms.controller.storage;
 
+import com.ssg.meowwms.dto.category.CategoryDTO;
 import com.ssg.meowwms.dto.category.MainCategoryDTO;
 import com.ssg.meowwms.dto.category.MiddleCategoryDTO;
 import com.ssg.meowwms.dto.category.SubCategoryDTO;
+import com.ssg.meowwms.dto.warehouse.WarehouseDTO;
 import com.ssg.meowwms.service.category.CategoryService;
+import com.ssg.meowwms.service.warehouse.WarehouseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,6 +26,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class StorageController {
     private final CategoryService categoryService;
+    private final WarehouseService warehouseService;
 
     /**
      * 입고 요청 페이지를 불러옵니다.
@@ -57,13 +61,34 @@ public class StorageController {
         return middleCategories;
     }
 
+    /**
+     * 주어진 대분류, 중분류와 일치하는 소분류 데이터를 가져옵니다.
+     *
+     * @param mainCategory
+     * @param middleCategory
+     * @return 소분류 리스트
+     */
     @GetMapping("/getSubCategories")
-    public @ResponseBody List<String> getSubCategories(@RequestParam("mainCategory") String mainCategory, @RequestParam("middleCategory") String middleCategory) {
+    public @ResponseBody List<String> getSubCategories(
+            @RequestParam("mainCategory") String mainCategory,
+            @RequestParam("middleCategory") String middleCategory) {
         List<String> subCategories = new ArrayList<>();
 
         List<SubCategoryDTO> subCategoryDTOList = categoryService.getSubCategories(mainCategory, middleCategory);
         subCategoryDTOList.forEach(subCategoryDTO -> subCategories.add(subCategoryDTO.getSubCategory()));
 
         return subCategories;
+    }
+
+    @GetMapping("/getWarehouseWithCategory")
+    public @ResponseBody List<WarehouseDTO> getWarehouseWithCategory(
+            @RequestParam("mainCategory") String mainCategory,
+            @RequestParam("middleCategory") String middleCategory,
+            @RequestParam("subCategory") String subCategory) {
+
+        CategoryDTO categoryDTO = categoryService.getWithCategories(mainCategory, middleCategory, subCategory);
+        int categoryId = categoryDTO.getId();
+
+        return warehouseService.getWarehouseWithCategory(categoryId);
     }
 }
