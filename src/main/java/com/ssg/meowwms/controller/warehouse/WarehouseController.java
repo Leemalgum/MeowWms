58e3809a36366aa462e4135d877018372474747b
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-25 22:31:04 +0900

    feat: WarehouseController 클래스 getRegister 메서드 카테고리 데이터를 창고 등록 페이지로 전달하는 로직 구현

--- src/main/java/com/ssg/meowwms/controller/warehouse/WarehouseController.java
+++ src/main/java/com/ssg/meowwms/controller/warehouse/WarehouseController.java
@@ -1,9 +1,15 @@
 package com.ssg.meowwms.controller.warehouse;
 
+import com.ssg.meowwms.dto.category.MainCategoryDTO;
+import com.ssg.meowwms.service.category.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 창고 관리
@@ -12,14 +18,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/warehouse")
 @RequiredArgsConstructor
 public class WarehouseController {
+    private final CategoryService categoryService;
 
     /**
      * 창고 등록 페이지를 불러옵니다.
      *
+     * @param model
      * @return
      */
     @GetMapping("/register")
-    public String getRegister() {
+    public String getRegister(Model model) {
+        List<String> mainCategories = new ArrayList<>();
+
+        List<MainCategoryDTO> mainCategoryDTOList = categoryService.getMainCategories();
+        mainCategoryDTOList.forEach(mainCategoryDTO -> mainCategories.add(mainCategoryDTO.getMainCategory()));
+
+        model.addAttribute("mainCategories", mainCategories);
+
         return "views/warehouse/register";
     }
 
