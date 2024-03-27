4951833ef7649cb6594d60fccb7a589d5764e550
Leemalgum <withxia1215@naver.com>
2024-03-27 09:24:33 +0900

    Fix: 전체 회원 조회 페이지 모달에서 권한/가입상태 안보이던 부분 수정

--- src/main/java/com/ssg/meowwms/config/SecurityConfig.java
+++ src/main/java/com/ssg/meowwms/config/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
